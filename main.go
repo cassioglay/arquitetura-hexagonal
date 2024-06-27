110b89eb7f5bf58b71584003c4df779678701f18
cassioglay <cassio.glay#locktec.com.br>
2024-06-27 13:33:40 -0300

    feat: Command CLI - exec cli using cobra

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-
-	db2 "github.com/cassioglay/arquitetura-hexagonal/adapters/db"
-	"github.com/cassioglay/arquitetura-hexagonal/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/cassioglay/arquitetura-hexagonal/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "db.sqlite")
-	productDbApter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbApter)
-	product, _ := productService.Create("Product Exemplo", 10)
-
-	productService.Enable(product)
-
+	cmd.Execute()
 }
